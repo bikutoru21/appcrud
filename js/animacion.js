5668f957eca220f9003a98af479dbcbf34f1bdb1
Victor_Perez <lupi_vic_94@hotmail.com>
2023-05-21 15:37:54 -0700

    Edicion de codigo numero 2

--- js/animacion.js
+++ js/animacion.js
@@ -1,104 +1,148 @@
-const createForm = document.getElementById('create-form');
+const form = document.getElementById('crud-form');
 const nameInput = document.getElementById('name-input');
-const descriptionInput = document.getElementById('description-input');
-const itemList = document.getElementById('item-list');
+const phoneInput = document.getElementById('phone-input');
+const emailInput = document.getElementById('email-input');
+const positionInput = document.getElementById('position-input');
+const employeeList = document.getElementById('employee-list');
 
-let items = [];
-let editingItemId = null;
+let employees = [];
+let updateButton = null;
 
-function createItem(event) {
+function addEmployee(event) {
   event.preventDefault();
 
   const name = nameInput.value.trim();
-  const description = descriptionInput.value.trim();
+  const phone = phoneInput.value.trim();
+  const email = emailInput.value.trim();
+  const position = positionInput.value.trim();
 
-  if (name === '' || description === '') {
-    alert('Por favor, ingresa un nombre y una descripción para el elemento.');
+  if (name === '' || phone === '' || email === '' || position === '') {
+    showError('Por favor, completa todos los campos');
     return;
   }
 
-  if (editingItemId) {
-    // Editar el elemento existente
-    items = items.map(item => {
-      if (item.id === editingItemId) {
-        return { id: item.id, name, description };
-      }
-      return item;
-    });
+  const employee = {
+    id: Date.now(),
+    name,
+    phone,
+    email,
+    position
+  };
 
-    editingItemId = null;
-  } else {
-    // Crear un nuevo elemento
-    const item = {
-      id: Date.now(),
-      name,
-      description
-    };
-  
-    items.push(item);
-  }
+  employees.push(employee);
+  renderEmployeeList();
 
-  renderItems();
-  createForm.reset();
+  form.reset();
 }
 
-function deleteItem(id) {
-  items = items.filter(item => item.id !== id);
-  renderItems();
-}
+form.addEventListener('submit', addEmployee);
 
-function editItem(id) {
-  const item = items.find(item => item.id === id);
-  if (item) {
-    nameInput.value = item.name;
-    descriptionInput.value = item.description;
-    editingItemId = id;
-  }
-}
+function renderEmployeeList() {
+  employeeList.innerHTML = '';
 
-function renderItems() {
-  itemList.innerHTML = '';
+  employees.forEach(employee => {
+    const row = document.createElement('tr');
 
-  if (items.length === 0) {
-    const emptyMessage = document.createElement('p');
-    emptyMessage.textContent = 'No hay elementos registrados.';
-    itemList.appendChild(emptyMessage);
-    return;
-  }
+    const nameCell = document.createElement('td');
+    nameCell.textContent = employee.name;
+    row.appendChild(nameCell);
 
-  items.forEach(item => {
-    const listItem = document.createElement('li');
-    listItem.className = 'list-item';
+    const phoneCell = document.createElement('td');
+    phoneCell.textContent = employee.phone;
+    row.appendChild(phoneCell);
 
-    const itemName = document.createElement('h3');
-    itemName.textContent = item.name;
-    listItem.appendChild(itemName);
+    const emailCell = document.createElement('td');
+    emailCell.textContent = employee.email;
+    row.appendChild(emailCell);
 
-    const itemDescription = document.createElement('p');
-    itemDescription.textContent = item.description;
-    listItem.appendChild(itemDescription);
+    const positionCell = document.createElement('td');
+    positionCell.textContent = employee.position;
+    row.appendChild(positionCell);
 
-    const buttonContainer = document.createElement('div');
-    buttonContainer.className = 'button-container';
-
+    const actionsCell = document.createElement('td');
     const editButton = document.createElement('button');
     editButton.textContent = 'Editar';
-    editButton.className = 'edit-button';
-    editButton.addEventListener('click', () => editItem(item.id));
-    buttonContainer.appendChild(editButton);
+    editButton.addEventListener('click', () => editEmployee(employee.id));
+    actionsCell.appendChild(editButton);
 
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Eliminar';
-    deleteButton.className = 'delete-button';
-    deleteButton.addEventListener('click', () => deleteItem(item.id));
-    buttonContainer.appendChild(deleteButton);
+    deleteButton.addEventListener('click', () => deleteEmployee(employee.id));
+    actionsCell.appendChild(deleteButton);
 
-    listItem.appendChild(buttonContainer);
+    row.appendChild(actionsCell);
 
-    itemList.appendChild(listItem);
+    employeeList.appendChild(row);
   });
 }
 
-createForm.addEventListener('submit', createItem);
+function deleteEmployee(id) {
+  employees = employees.filter(employee => employee.id !== id);
+  renderEmployeeList();
+}
 
-renderItems();
+function editEmployee(id) {
+  const employee = employees.find(employee => employee.id === id);
+
+  if (employee) {
+    nameInput.value = employee.name;
+    phoneInput.value = employee.phone;
+    emailInput.value = employee.email;
+    positionInput.value = employee.position;
+
+    // Si ya existe un botón "Actualizar", no se crea otro
+    if (!updateButton) {
+      updateButton = document.createElement('button');
+      updateButton.textContent = 'Actualizar';
+      updateButton.addEventListener('click', () => updateEmployee(id));
+      form.appendChild(updateButton);
+    }
+
+    deleteEmployee(id); // Elimina el empleado existente para evitar duplicados
+  }
+}
+
+function updateEmployee(id) {
+  const name = nameInput.value.trim();
+  const phone = phoneInput.value.trim();
+  const email = emailInput.value.trim();
+  const position = positionInput.value.trim();
+
+  if (name === '' || phone === '' || email === '' || position === '') {
+    showError('Por favor, completa todos los campos');
+    return;
+  }
+
+  const employee = {
+    id,
+    name,
+    phone,
+    email,
+    position
+  };
+
+  employees.push(employee);
+  renderEmployeeList();
+
+  form.reset();
+
+  // Remover el botón de actualizar
+  if (updateButton) {
+    updateButton.remove();
+    updateButton = null; // Restablecer la variable para permitir la creación de otro botón en la próxima edición
+  }
+}
+
+function showError(message) {
+  const errorElement = document.createElement('div');
+  errorElement.classList.add('error');
+  errorElement.textContent = message;
+
+  form.appendChild(errorElement);
+
+  setTimeout(() => {
+    errorElement.remove();
+  }, 3000);
+}
+
+renderEmployeeList();
